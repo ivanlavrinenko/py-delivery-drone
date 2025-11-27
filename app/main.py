6439b1b3986555b5a0c990bf28e40f6d45a7c078
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2025-11-27 04:00:47 +0100

    redundant assignment removed

--- app/main.py
+++ app/main.py
@@ -51,10 +51,9 @@ class DeliveryDrone(FlyingRobot):
                  weight: int,
                  max_load_weight: int,
                  coords: list[int] | None = None,
-                 current_load: Cargo = None) -> None:
+                 current_load: Cargo | None = None) -> None:
         super().__init__(name=name, weight=weight, coords=coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
         self.current_load = None
         if current_load is not None:
             self.hook_load(current_load)
